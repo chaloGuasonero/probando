e3f17acae6818f03a36059c62ed179f98615b884
EnriqueM <enriqueguasonero@gmail.com>
2023-11-15 10:58:30 -0300

    Agregue un input apellido y lo muestro en preferencias.php junto con el nombre

---
+++ preferencias.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preferencias</title>
+</head>
+<body>
+    
+<?php
+
+    $name=$_GET['name'];
+    $surname=$_GET['surname'];
+
+    echo $name." ".$surname;
+
+
+?>
+
+
+
+
+
+</body>
+</html>
